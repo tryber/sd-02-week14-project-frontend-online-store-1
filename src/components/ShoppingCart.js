944c0ca14e905a8a19aae4be93136e512b7c81c1
Giorgia <giorgialeoniac@gmail.com>
2020-02-17 18:08:30 -0300

    Mudanças cc

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -10,14 +10,14 @@ class ShoppingCart extends React.Component {
     render() {
         return ( 
         <div>
-            <img src={BackArrow} alt="Seta para o lado esquerdo indicando para voltar a página" className="seta"/>
-              <div className="meuCarrinho">
-                <img src={Cart} alt="Imagem de um carrinho de compras" className="carrinho"/>
-                <p className="textoCarrinho">Carrinho de Compras</p>
-              </div>
-              <div className="minhaCaixa">
-                <img src={EmptyBox} alt="Imagem de uma caixa vazia indicando que não há itens no carrinho" className="caixa"/>
-                <p className="textoCaixa">Seu Carrinho Está Vazio</p>
+            <img src={BackArrow} alt="Seta para o lado esquerdo indicando para voltar a página" className="seta" />
+            <div className="meuCarrinho">
+            <img src={Cart} alt="Imagem de um carrinho de compras" className="carrinho" />
+            <p className="textoCarrinho">Carrinho de Compras</p>
+            </div>
+            <div className="minhaCaixa">
+            <img src={EmptyBox} alt="Imagem de caixa vazia indicando que não há itens no carrinho" className="caixa" />
+            <p className="textoCaixa">Seu Carrinho Está Vazio</p>
             </div>
         </div>);
     }
