dd60c29e72ba407d5c0cb6c39f57b813d6945808
phtf92 <phtf92@gmail.com>
2020-02-17 16:12:41 -0300

    Code climate

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,34 +2,34 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-          searchItem: '',
-        };
-      this.handleChange = this.handleChange.bind(this);
-    }
+    super(props);
+    this.state = {
+        searchItem: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   handleChange(event) {
-      const { value } = event.target;
-      this.setState({ searchItem: value });
-    }
+    const { value } = event.target;
+    this.setState({ searchItem: value });
+  }
 
   createInput() {
-      return (
-          <input
-              type="text"
-              id="searchBar"
-              value={this.state.searchItem}
-              onChange={this.handleChange}
-            />
-        );
-    }
+    return (
+        <input
+            type="text"
+            id="searchBar"
+            value={this.state.searchItem}
+            onChange={this.handleChange}
+          />
+      );
+  }
 
   didSearch() {
-      const { searchItem } = this.state;
-      if (!searchItem) return <div>Você ainda não realizou uma busca</div>;
-      return <div></div>
-    }
+    const { searchItem } = this.state;
+    if (!searchItem) return <div>Você ainda não realizou uma busca</div>;
+    return <div />;
+  }
 
   render() {
       return (
