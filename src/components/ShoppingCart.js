6b71329ca54b33155b0433f9fcedc163c71e620c
Giorgia <giorgialeoniac@gmail.com>
2020-02-17 18:38:56 -0300

    Correções CC

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -11,7 +11,9 @@ class ShoppingCart extends React.Component {
     render() {
         return (
             <div>
-                <img src={BackArrow} alt="Seta para voltar à página inicial" className="seta" Link to="/"/>
+                <Link to="/">
+                    <img src={BackArrow} alt="Seta para voltar à página inicial" className="seta" />
+                </Link>
                 <div className="meuCarrinho">
                     <img src={Cart} alt="Imagem de um carrinho de compras" className="carrinho" />
                     <p className="textoCarrinho">Carrinho de Compras</p>
