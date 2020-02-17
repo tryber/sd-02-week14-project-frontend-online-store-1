ac7e58a5f7ce222862b86074ec9c68eb8b58fafd
phtf92 <phtf92@gmail.com>
2020-02-17 15:23:50 -0300

    Code Climate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Inicio Projeto</div>
-      <Switch>
-      </Switch>
+      <Switch />
     </Router>
   );
 }
