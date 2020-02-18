27d3a0dbc36424d58fd080006fec14b8bceaaa64
Giorgia <giorgialeoniac@gmail.com>
2020-02-18 18:55:15 -0300

    Envio do CSS + Testando cc

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Product.css';
+import BackArrow from '../imgs/back-arrow.png';
+import Cart from '../imgs/img_290616.png';
 
 class Product extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    geraProduto() {
-    const { title, price, thumbnail } = this.props.location.state.item;
-      return (
-          <div className="productContainer">
-            <div className="titulo">{title} - {price}</div>
-            <img src={thumbnail} className="productImage" alt="Imagem do produto clicado" />
-            <div className="especificacoesTecnicas"></div>
-          </div>
-      )
-    }
-
-    render() {
-    console.log(this.props.location.state.item);
-    return (
-    <div>
-    {this.geraProduto(this.props.location.state)}
-    </div>
+  geraProduto() {
+  const { title, price, thumbnail, attributes } = this.props.location.state.item; 
+  return (
+      <div className="productContainer">
+      <div className="titulo">{title} - R${price}</div>
+      <div className="display">
+      <img src={thumbnail} className="productImage" alt="Imagem do produto clicado" />
+      <div className="especificacoesTecnicas">
+      <p>Especificações Técnicas</p>
+      {attributes.map(atributos => (
+        <li key={atributos.name}>{atributos.name}: {atributos.value_name}</li>
+      ))}
+      </div>
+      </div>
+      </div>
     )
-    }
+  }
+
+  render() {
+  return (
+  <div className="body">
+  <div className="icons">
+  <Link to="/">
+  <img src={BackArrow} alt="Seta para voltar à página anterior" className="seta" />
+  </Link>
+  <Link to="/shopping-cart">
+  <img src={Cart} alt="Carrinho de compras" className="carrinho" />
+  </Link>
+  </div>
+  {this.geraProduto(this.props.location.state)}
+  </div>
+  )
+  }
 }
 
 export default Product;
