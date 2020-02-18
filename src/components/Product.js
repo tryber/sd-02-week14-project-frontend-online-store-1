873403384615d62a1de29e155d3be5505dbd2406
Giorgia <giorgialeoniac@gmail.com>
2020-02-18 18:13:24 -0300

    Conseguimos colocar o produto na página

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,9 +6,24 @@ class Product extends React.Component {
         this.state = {};
     }
 
+    geraProduto() {
+    const { title, price, thumbnail } = this.props.location.state.item;
+      return (
+          <div className="productContainer">
+            <div className="titulo">{title} - {price}</div>
+            <img src={thumbnail} className="productImage" alt="Imagem do produto clicado" />
+            <div className="especificacoesTecnicas"></div>
+          </div>
+      )
+    }
+
     render() {
-        console.log(this.props.location.state)
-        return (<p>Olá</p>)
+    console.log(this.props.location.state.item);
+    return (
+    <div>
+    {this.geraProduto(this.props.location.state)}
+    </div>
+    )
     }
 }
 
