73e7c053f33dde1acf7b558758009652f0631798
Ishidor <kevinmendessilva@hotmail.com>
2020-02-18 17:01:44 -0300

    teste pronto

---
+++ src/components/Categories.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+
+class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.renderCategory = this.renderCategory.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://api.mercadolibre.com/sites/MLB/categories')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data });
+      });
+  }
+
+  handleChange(event) {
+    const { onChange } = this.props;
+    onChange(event);
+  }
+
+  renderCategory(id, value) {
+    return (
+      <label htmlFor={id} key={id} className="category-options">
+        <input
+          name="option"
+          type="radio"
+          id={id}
+          key={id}
+          value={id}
+          onChange={this.handleChange}
+        />
+        <div>{value}</div>
+      </label>
+    );
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="category-box">
+        <h3>Categorias</h3>
+        <div className="category-options-box">
+          {data.map((dat) => this.renderCategory(dat.id, dat.name))}
+        </div>
+      </div>
+    );
+  }
+}
+
+Categories.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Categories;
