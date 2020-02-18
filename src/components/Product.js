3d264bb9fa7ca7ef0a5d2232ebbe05aa3029b68d
Giorgia <giorgialeoniac@gmail.com>
2020-02-18 19:18:27 -0300

    Correções CC

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,20 +6,20 @@ import Cart from '../imgs/img_290616.png';
 
 class Product extends React.Component {
   constructor(props) {
-  super(props);
-  this.state = {};
+    super(props);
+    this.state = {};
   }
 
   geraProduto() {
-  const { title, price, thumbnail, attributes } = this.props.location.state.item;
-  return (
+    const { title, price, thumbnail, attributes } = this.props.location.state.item;
+    return (
     <div className="productContainer">
     <div className="titulo">{title} - R${price}</div>
     <div className="display">
     <img src={thumbnail} className="productImage" alt="Imagem do produto clicado" />
     <div className="especificacoesTecnicas">
     <p>Especificações Técnicas</p>
-    {attributes.map(atributos => (
+      {attributes.map((atributos) => (
     <li key={atributos.name}>{atributos.name}: {atributos.value_name}</li>
     ))}
     </div>
@@ -31,15 +31,15 @@ class Product extends React.Component {
   render() {
     return (
     <div className="body">
-    <div className="icons">
-    <Link to="/">
-    <img src={BackArrow} alt="Seta para voltar à página anterior" className="seta" />
-    </Link>
-    <Link to="/shopping-cart">
-    <img src={Cart} alt="Carrinho de compras" className="carrinho" />
+      <div className="icons">
+      <Link to="/">
+      <img src={BackArrow} alt="Seta para voltar à página anterior" className="seta" />
+      </Link>
+      <Link to="/shopping-cart">
+      <img src={Cart} alt="Carrinho de compras" className="carrinho" />
     </Link>
     </div>
-    {this.geraProduto(this.props.location.state)}
+      {this.geraProduto(this.props.location.state)}
     </div>
     );
   }
