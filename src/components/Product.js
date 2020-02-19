babd8f9be28e7b8d7b0d442ada5b0c3246a8ac7a
Ishidor <kevinmendessilva@hotmail.com>
2020-02-19 19:29:46 -0300

    Só salvando

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import './Product.css';
 import BackArrow from '../imgs/back-arrow.png';
 import Cart from '../imgs/img_290616.png';
+import AddToCartBtn from './addToCartBtn';
 
 class Product extends React.Component {
   constructor(props) {
@@ -25,6 +26,7 @@ class Product extends React.Component {
           ))}
           </div>
         </div>
+        <AddToCartBtn />
       </div>
     );
   }
