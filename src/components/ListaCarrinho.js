62087d1a19884c56841d0b0f0f9973a0fb1a3920
phtf92 <phtf92@gmail.com>
2020-02-20 18:08:23 -0300

    renderizando carrinho vazio

---
+++ src/components/ListaCarrinho.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ListaCarrinho extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itens: [],
+    };
+  }
+  render() {
+    return (
+      <div>Oie</div>
+    )
+  }
+}
+
+export default ListaCarrinho;
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ListaCarrinho from './ListaCarrinho';
 import BackArrow from '../imgs/back-arrow.png';
 import Cart from '../imgs/img_290616.png';
 import EmptyBox from '../imgs/empty.png';
@@ -7,22 +8,42 @@ import './ShoppingCart.css';
 
 class ShoppingCart extends React.Component {
 
+  listaCheia() {
+    if (localStorage.getItem('products')) {
+      return (
+      <div>
+        <ListaCarrinho />
+    </div>)
+      }
+    }
+    
+  listaVazia() {
+    if (!localStorage.getItem('products')) {
+      return (
+        <div>
+          <Link to="/">
+            <img src={BackArrow} alt="Seta para voltar à página inicial" className="seta" />
+          </Link>
+          <div className="meuCarrinho">
+            <img src={Cart} alt="Imagem de um carrinho de compras" className="carrinho" />
+            <p className="textoCarrinho">Carrinho de Compras</p>
+          </div>
+          <div className="minhaCaixa">
+            <img src={EmptyBox} alt="Imagem de caixa vazia" className="caixa" />
+            <p className="textoCaixa">Seu Carrinho Está Vazio</p>
+          </div>
+        </div>);
+    }
+  }
+
   render() {
     return (
       <div>
-        <Link to="/">
-          <img src={BackArrow} alt="Seta para voltar à página inicial" className="seta" />
-        </Link>
-        <div className="meuCarrinho">
-          <img src={Cart} alt="Imagem de um carrinho de compras" className="carrinho" />
-          <p className="textoCarrinho">Carrinho de Compras</p>
+          {this.listaVazia()}
+          {this.listaCheia()}
         </div>
-        <div className="minhaCaixa">
-          <img src={EmptyBox} alt="Imagem de caixa vazia" className="caixa" />
-          <p className="textoCaixa">Seu Carrinho Está Vazio</p>
-        </div>
-      </div>);
-  }
-}
-
-export default ShoppingCart;
+        )
+      }
+    }
+    
+    export default ShoppingCart;
