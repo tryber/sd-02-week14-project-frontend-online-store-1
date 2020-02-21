625f34bc576126b6399c333b44a33690d0f4085e
phtf92 <phtf92@gmail.com>
2020-02-21 15:01:16 -0300

    Code Climate

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     const objProdutos = JSON.parse(localStorage.getItem('products'));
-    const ids = Object.keys(objProdutos).length;
+    const ids = objProdutos ? Object.keys(objProdutos).length : 0;
     this.state = {
       searchItem: '',
       searchCategorie: '',
