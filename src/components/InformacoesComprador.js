57ffb70a7be5405dbaa544fb95ccb03e39145e1f
phtf92 <phtf92@gmail.com>
2020-02-26 18:02:09 -0300

    Borda vermelha quando não digitei

--- src/components/InformacoesComprador.js
+++ src/components/InformacoesComprador.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import states from './estados';
+import './InformacoesComprador.css';
 
 class InfoComprador extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Nome: '',
+      CPF: '',
+      Email: '',
+      Telefone: '',
+      CEP: '',
+      Endereço: '',
+      Complemento: '',
+      Número: '',
+      Cidade: '',
+    }
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    const { value, name } = event.target
+    if (!value) {
+      this.setState({ [name]: 'incompleto' })
+    }
+    else { this.setState({ [name]: value }) }
+  }
 
   criarInput(type, name, placeholder) {
-    console.log(this.props);
+    const nomear = this.state[name]
+    let nomeDaClasse = 'normal'
+    if (nomear === 'incompleto') {
+      nomeDaClasse = 'vermelho' 
+    }
     return (
       <input
         type={type}
         name={name}
         placeholder={placeholder}
+        onChange={this.handleInput}
+        className={nomeDaClasse}
       />
     );
   }
   render() {
     const estados = states;
+    console.log(this.state)
     return (
       <div>
         <form>
-          {this.criarInput('text', 'nome', 'Nome Completo')}
+          {this.criarInput('text', 'Nome', 'Nome Completo')}
           {this.criarInput('text', 'CPF', 'CPF')}
           {this.criarInput('email', 'Email', 'Email')}
           {this.criarInput('text', 'Telefone', 'Telefone')}
