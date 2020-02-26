56016a267b496c72829f7a212b3a854903bdd0a5
phtf92 <phtf92@gmail.com>
2020-02-26 15:32:15 -0300

    informacoe do comprador

--- src/components/InformacoesComprador.js
+++ src/components/InformacoesComprador.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import states from './estados';
 
 class InfoComprador extends React.Component {
 
+  criarInput(type, name, placeholder) {
+    return (
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+      />
+    );
+  }
   render() {
+    const estados = states;
     return (
       <div>
-        <p>InfoComprador</p>
+        <form>
+          {this.criarInput('text', 'nome', 'Nome Completo')}
+          {this.criarInput('text', 'CPF', 'CPF')}
+          {this.criarInput('email', 'Email', 'Email')}
+          {this.criarInput('text', 'Telefone', 'Telefone')}
+          {this.criarInput('text', 'CEP', 'CEP')}
+          {this.criarInput('text', 'Endereço', 'Endereço')}
+          {this.criarInput('number', 'Complemento', 'Complemento')}
+          {this.criarInput('number', 'Número', 'Número')}
+          {this.criarInput('text', 'Cidade', 'Cidade')}
+          <select name="estado" >
+            <option>Estado</option>
+            {estados.map((estado) => (
+              <option
+                key={Object.keys(estado)}
+                value={Object.keys(estado)}
+              >
+                {Object.values(estado)}
+              </option>
+            ))}
+          </select>
+        </form>
       </div>
     )
   }
